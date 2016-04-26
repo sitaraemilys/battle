ea9d274273b7c01e687c5d01fa052b1a12594a50
sitypop <sityemilyshah@gmail.com>
2016-04-26 16:47:57 +0100

    Attack button and test

--- app.rb
+++ app.rb
@@ -21,7 +21,11 @@ class Battle < Sinatra::Base
     erb(:play)
   end
 
-
+  get '/attack' do
+    @player1_name = session[:player1_name]
+    @player2_name = session[:player2_name]
+    erb(:attack)
+  end
 
 
   # start the server if ruby file executed directly
--- spec/features/capybara_feature_spec.rb
+++ spec/features/capybara_feature_spec.rb
@@ -23,3 +23,11 @@ feature 'Display hit points' do
     expect(page).to have_text("Player Two HP: 20")
   end
 end
+
+feature 'Attack button' do
+  scenario "player one can attack player two" do
+    sign_in_and_play
+    click_button('Attack!')
+    expect(page).to have_text("Sity attacked Alex!")
+  end
+end
