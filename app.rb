fce16558dea86fdbcfd96f75894e7250c2e0d711
Elia Bardashevich <ebardashevich@gmail.com>
2016-04-26 16:49:03 +0100

    adds app.rb tests in spec file and user story 1 implemented

--- app.rb
+++ app.rb
@@ -1,10 +1,15 @@
 require 'sinatra/base'
-
+enable :sesions 
 class Battle < Sinatra::Base
   get '/' do
-    'Hello Battle!'
+    'Hello Battle! enter your name please'
+    erb(:index)
   end
-
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb(:play)
+  end
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
