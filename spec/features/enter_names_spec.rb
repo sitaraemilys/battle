a964c1ac05d4cba494a6452bdc88873beb9cfc8e
Ruta <ruta@baltiejus.com>
2016-04-26 16:00:56 +0100

    Beginning to implement user stories as well as feature tests, needing to refactor tests

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -5,6 +5,16 @@ feature 'Enter names' do
     fill_in :player_1_name, with: 'Hanna'
     fill_in :player_2_name, with: 'Ruta'
     click_button 'Submit'
-    expect(page).to have_content 'Hanna vs Ruta'
+    expect(page).to have_content 'Hanna vs. Ruta'
+  end
+end
+
+feature 'View hit points' do
+  scenario 'See player 2s hitpoints' do
+    visit ('/')
+    fill_in :player_1_name, with: 'Hanna'
+    fill_in :player_2_name, with: 'Ruta'
+    click_button 'Submit'
+    expect(page).to have_content 'Ruta: 100HP'
   end
 end
