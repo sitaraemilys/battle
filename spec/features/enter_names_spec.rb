8628ae82df8e0375c582cae203b41b25abe47769
Hanna <hannaeberhoefer@yahoo.com>
2016-04-26 13:24:47 +0100

    First feature test passed

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,10 @@
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Hanna'
+    fill_in :player_2_name, with: 'Ruta'
+    click_button 'Submit'
+    expect(page).to have_content 'Hanna vs Ruta'
+  end
+end
