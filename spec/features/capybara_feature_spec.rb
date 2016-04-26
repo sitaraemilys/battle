26336df6e6e26d9718928ebac26fd3228380c921
Alex Robertson <lex1989@gmail.com>
2016-04-26 16:07:47 +0100

    Created web_helpers.rb

--- spec/features/capybara_feature_spec.rb
+++ spec/features/capybara_feature_spec.rb
@@ -1,41 +1,25 @@
 
 feature 'Enter names' do
   scenario "player one can submit name" do
-    visit "/"
-
-    fill_in "player1_name", :with => "Alex"
-    click_button "Submit"
-
-    expect(page).to have_text("Player One: Alex")
+    sign_in_and_play
+    expect(page).to have_text("Player One: Sity")
   end
 
   scenario "player two can submit name" do
-    visit "/"
-
-    fill_in "player2_name", :with => "Sity"
-    click_button "Submit"
-
-    expect(page).to have_text("Player Two: Sity")
+    sign_in_and_play
+    expect(page).to have_text("Player Two: Alex")
   end
 
 end
 
 feature 'Display hit points' do
   scenario "player one starts with 20 hit points" do
-    visit "/"
-
-    fill_in "player1_name", :with => "Alex"
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_text("Player One HP: 20")
   end
 
   scenario "player two starts with 20 hit points" do
-    visit "/"
-
-    fill_in "player2_name", :with => "Sity"
-    click_button "Submit"
-
+    sign_in_and_play
     expect(page).to have_text("Player Two HP: 20")
   end
 end
