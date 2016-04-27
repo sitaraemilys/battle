b6e535bdc61e9e43a2fb8ee3af3d1c310bab76d8
Hanna <hannaeberhoefer@yahoo.com>
2016-04-27 12:39:04 +0100

    Switching turns

---
+++ spec/features/switch_turns_spec.rb
@@ -0,0 +1,18 @@
+
+
+describe 'Switch turns' do
+
+  context 'seeing the current turn' do
+    scenario 'at the start o the game' do
+      sign_in_and_play
+      expect(page).to have_content "Hanna's turn"
+    end
+
+    scenario 'after player 1 attacks' do
+      sign_in_and_play
+      click_link 'Attack'
+      expect(page).not_to have_content "Hanna's turn"
+      expect(page).to have_content "Ruta's turn"
+    end
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -19,12 +19,26 @@ describe Game do
     end
   end
 
-
   describe '#attack' do
     it 'damages the player' do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
     end
   end
+
+  describe '#current_turn' do
+    it 'starts as player 1' do
+      expect(game.current_turn).to eq player_1
+    end
+  end
+
+  describe '#switch_turns' do
+    it 'switches the turn' do
+      game.switch_turns
+      expect(game.current_turn).to eq player_2
+    end
+  end
+
+
 
 end
